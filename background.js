87795853b9b8bf1c5831a711da889b106b12ae6d
BrandonRudiselNDSU <brandonrudisel@gmail.com>
2019-04-28 18:13:00 -0500

    fix background

--- background.js
+++ background.js
@@ -1,9 +1,15 @@
+
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 'use strict';
-chrome.runtime.onInstalled.addListener(function ());
+
+//we don't know what this does, but it came with the extension tutorial and we don't want to remove it
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.set({color: '#3aa757'}, function () {
+        console.log('The color is green.');
+    });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({})],
